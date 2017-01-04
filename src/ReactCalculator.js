f824e4df87f8546e62f15ce47ab69b25d0dd5064
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-04 22:49:08 +0100

    feat(calc): layout

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const ReactCalculator = () => (
-  <Text>Hello, React!</Text>
+  <View style={styles.container}>
+    <View style={styles.display}></View>
+    <View style={styles.buttons}></View>
+  </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  display: {
+    flex: 2,
+    backgroundColor: '#193441'
+  },
+  buttons: {
+    flex: 8,
+    backgroundColor: '#3E606F'
+  }
+})
 
 export default ReactCalculator;
