5181fc4f2e1d9d2495f20f0273e6d2baddcc17dc
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-04 21:09:47 +0100

    feat(calculator): declare new own component
    feat(calculator): change component name in AppDelegate

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,19 @@
  * @flow
  */
 
+ import React, { Component } from 'react';
+ import { Text, AppRegistry } from 'react-native';
+
+ class ReactCalculator extends Component {
+   render() {
+     return (
+       <Text>Hello, React!</Text>
+     );
+   }
+ }
+
+ AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
+
 /* import React, { Component } from 'react';
 import {
   AppRegistry,
