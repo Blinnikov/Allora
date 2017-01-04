77fd87630ec09938f27b925a59d8492743bc6236
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-04 22:29:58 +0100

    chore(calc): correct message

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 const ReactCalculator = () => (
-  <Text>Hello, React!++</Text>
+  <Text>Hello, React!</Text>
 );
 
 export default ReactCalculator;
