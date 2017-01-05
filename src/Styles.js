04f5391e72857fe8cd4976b8e2666b308a275eb0
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-05 21:27:02 +0100

    fix(calc): enclose color constant in quotes

--- src/Styles.js
+++ src/Styles.js
@@ -19,7 +19,7 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 0.5,
-    borderColor: #91AA9D
+    borderColor: '#91AA9D'
   },
   inputButtonText: {
     fontSize: 22,
