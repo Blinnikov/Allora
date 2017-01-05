a590b55fbd9c4bd2eea1db4c6d651b00443fdaef
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-05 21:28:33 +0100

    chore(calc): add some dummy buttons

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import InputButton from './InputButton';
 import Styles from './Styles';
 
 const ReactCalculator = () => (
   <View style={Styles.container}>
     <View style={Styles.display}></View>
-    <View style={Styles.buttons}></View>
+    <View style={Styles.buttons}>
+      <InputButton value={1} />
+      <InputButton value={2} />
+      <InputButton value={3} />
+      <InputButton value={4} />
+    </View>
   </View>
 );
 
