0f1a47814d407c1c82785e279af9ac57b0d8d523
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-05 22:47:52 +0100

    feat(calc): use button set component

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import InputButton from './InputButton';
+import InputButtonSet from './InputButtonSet';
 import Styles from './Styles';
+
+const rows = [
+  [1, 2, 3, '/'],
+  [4, 5, 6, '*'],
+  [7, 8, 9, '-'],
+  [0, '.', '=', '+']
+];
 
 const ReactCalculator = () => (
   <View style={Styles.container}>
     <View style={Styles.display}></View>
-    <View style={Styles.buttons}>
-      <InputButton value={1} />
-      <InputButton value={2} />
-      <InputButton value={3} />
-      <InputButton value={4} />
-    </View>
+    <InputButtonSet rows={rows} />
   </View>
 );
 
