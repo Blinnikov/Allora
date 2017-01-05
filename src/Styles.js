f25e8f8aedabd924272b5a3f81c23a56128f7a93
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-05 21:25:26 +0100

    feat(calc): styles for buttons

--- src/Styles.js
+++ src/Styles.js
@@ -11,6 +11,20 @@ const Styles = StyleSheet.create({
   buttons: {
     flex: 8,
     backgroundColor: '#3E606F'
+  },
+
+
+  inputButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    borderColor: #91AA9D
+  },
+  inputButtonText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'white'
   }
 });
 
