3048b8cc6063951246b0f8e0f2e83e96e222919f
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-09 22:25:25 +0300

    chore(calc): remove not used import

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
-import InputButton from './InputButton';
 import InputButtonSet from './InputButtonSet';
 import Styles from './Styles';
 
