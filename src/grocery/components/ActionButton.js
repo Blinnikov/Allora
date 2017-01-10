2b5dc0236151639b5137c125078624f4c5f7f43f
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-10 22:25:45 +0300

    feat(firebase): ActionButton w/o styles

--- src/grocery/components/ActionButton.js
+++ src/grocery/components/ActionButton.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+import { Text, View, TouchableHighlight } from 'react-native';
+
+import constants from '../constants';
+import styles from '../styles';
+
+class ActionButton extends Component {
+  render() {
+    return (
+      <View style={}>
+        <TouchableHighlight
+          underlayColor={constants.actionColor}
+          onPress={this.props.onPress}
+        >
+          <Text style={}>{this.props.title}</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+export default ActionButton;
