7acab80c18a83da80d60572cc3d8918a120201ad
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-10 22:16:06 +0300

    feat(firebase): styles for firebase app

---
+++ src/grocery/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    justifyContent: 'center'
+  }
+});
+
+export default styles;
