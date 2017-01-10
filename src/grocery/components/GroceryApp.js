1865694d3ef199cb169338f42edfb04e801c62d6
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-11 00:44:34 +0300

    feat(firebase): grocery app components layout

--- src/grocery/components/GroceryApp.js
+++ src/grocery/components/GroceryApp.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
+import StatusBar from './StatusBar';
+import ListItem from './ListItem';
+import ActionButton from './ActionButton';
 
 import styles from '../styles';
 
 class GroceryApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    }
+  }
+
+  _renderListItem(item) {
+    return (
+      <ListItem item={item} onPress={() => {}} />
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Firebase container
-        </Text>
+        <StatusBar />
+        <ListView
+          style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={this._renderListItem.bind(this)}
+        />
+        <ActionButton
+          title="Add"
+          onPress={() => {
+            alert('Add');
+          }}
+        />
       </View>
     );
   }
