e39a1aaf66b698e25a7f3d000c4b57ab01aa32a8
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-12 23:37:51 +0300

    feat(firebase): Login page + styles
    feat(firebase): add apsl button

---
+++ src/Styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8781BD',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 22
+  },
+  buttons: {
+    backgroundColor: 'whitesmoke'
+  },
+  buttonsText: {
+    fontSize: 18
+  }
+});
+
+export default Styles;
