0f22ec7221bc4717b28789223649473e096caad5
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-14 14:20:16 +0300

    feat(auth): Account page with logout functionality

--- src/auth/pages/Account.js
+++ src/auth/pages/Account.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as firebase from 'firebase';
 
-const Account = () => (
-  <View>
-    <Text>Account page..</Text>
-  </View>
-);
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Login from './Login'
+
+import styles from '../styles/common-styles';
+
+class Account extends Component {
+  constructor(props) {
+    super(props);
+
+    const user = firebase.auth().currentUser;
+    console.log(user);
+    this.state = {
+      user,
+      loaded: true
+    };
+  }
+
+  async logout() {
+    await firebase.auth().signOut();
+    this.props.navigator.push({
+      component: Login
+    })
+  }
+
+  render() {
+    const { user, loaded } = this.state;
+    return (
+      <View style={styles.container}>
+        <Header text='Account' loaded={loaded} />
+        <View style={styles.body}>
+          <View style={pageStyles.emailContaniner}>
+            <Text style={pageStyles.emailText}>{user.email}</Text>
+          </View>
+          <Button
+            text='Logout'
+            onPress={this.logout.bind(this)}
+            button_styles={styles.primaryButton}
+            button_text_styles={styles.primaryButtonText}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const pageStyles = StyleSheet.create({
+  emailContaniner: {
+    padding: 20
+  },
+  emailText: {
+    fontSize: 18
+  }
+})
 
 export default Account;
