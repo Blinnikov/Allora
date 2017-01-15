59ba33c70129bbb323bd664bbc16aed55fea8b60
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-15 17:04:47 +0300

    feat(navigation): add wrapper component for Account to handle nested navigation

---
+++ src/auth/pages/AccountContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Navigator } from 'react-native';
+
+import NavigationBarRouteMapper from '../components/NavigationBarRouteMapper';
+import Account from './Account';
+
+class AccountContainer extends Component {
+  render() {
+    const rootNavigator = this.props.navigator;
+    return (
+      <Navigator
+        initialRoute={{component: Account}}
+        configureScene={() => {
+          return Navigator.SceneConfigs.FloatFromRight;
+        }}
+        renderScene={(route, navigator) => {
+          if (route.component) {
+            return React.createElement(route.component, { navigator, rootNavigator });
+          }
+        }}
+        navigationBar={
+          <Navigator.NavigationBar
+            routeMapper={NavigationBarRouteMapper}
+          />
+        }
+      />
+    );
+  }
+}
+
+export default AccountContainer;
