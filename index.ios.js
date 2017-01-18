98a4780aaf6930c028e5b5e455558cd4462f0477
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-18 22:55:56 +0300

    chore: remove unused links from entry point

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,6 @@
 import { AppRegistry } from 'react-native';
 import Firebase from './src/firebase/firebase';
 
-import ReactCalculator from './src/calculator/ReactCalculator';
-import GroceryApp from './src/grocery/components/GroceryApp';
 import AuthApp from './src/auth/pages/AuthApp';
 
 Firebase.init();
