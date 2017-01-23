952eac9840b35fd53fd60ca0351a8f6258c2f7e3
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-23 23:14:36 +0300

    feat(firebase): add operations to add/update word

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -6,4 +6,23 @@ const getItemsRef = () => {
   return firebase.database().ref(path);
 }
 
-export { getItemsRef };
+const addWordItem = ({word = '', translation = '', lang}) => {
+  getItemsRef().push({
+    word,
+    translation,
+    lang,
+    added: Date.now()
+  });
+}
+
+const updateWordItem = (key, {word, translation, lang}) => {
+  getItemsRef().child(key)
+    .update({
+      word,
+      translation,
+      lang,
+      updated: Date.now()
+    });
+}
+
+export { getItemsRef, addWordItem, updateWordItem };
