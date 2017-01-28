8ec2967ca9a382bc8417d7676935595339054402
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-01-28 22:16:36 +0300

    feat(words):getRandomWord

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -25,4 +25,29 @@ const updateWordItem = (key, {word, translation, lang}) => {
     });
 }
 
-export { getItemsRef, addWordItem, updateWordItem };
+const getRandomWord = () => {
+  let result;
+
+  getItemsRef().on('value', snap => {
+    let i = 0;
+    const rand = Math.floor(Math.random() * snap.numChildren());
+
+    snap.forEach(record => {
+      if (i === rand) {
+        const { word, translation, lang } = record.val();
+        result = {
+          key: record.key,
+          word,
+          translation,
+          lang,
+        };
+      }
+
+      i++;
+    });
+  });
+
+  return result;
+}
+
+export { getItemsRef, addWordItem, updateWordItem, getRandomWord };
