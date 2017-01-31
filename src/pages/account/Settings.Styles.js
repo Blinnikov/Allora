899496568a446de6c499b0630b4b26de1e5d02ee
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-01 00:26:02 +0300

    feat(settings): enable notification setting

---
+++ src/pages/account/Settings.Styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const SettingsStyles = StyleSheet.create({
+  form: {
+    flex: 1
+  },
+});
+
+export default SettingsStyles;
--- src/pages/account/Settings.js
+++ src/pages/account/Settings.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Switch, Text, View } from 'react-native';
+import { FormLabel, FormInput, List, ListItem } from 'react-native-elements';
+import I18n from 'react-native-i18n';
+import UserSettings from '../../services/UserSettings';
 
 import CommonStyles from '../../styles/Common';
+import PageStyles from './Settings.Styles';
 
-const Settings = () => {
-  return (
-    <View style={CommonStyles.fullHeightContainer}>
-      <Text>Settings Page</Text>
-    </View>
-  );
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enableNotifications: false
+    };
+    this.fillInitialState()
+  }
+
+  async fillInitialState() {
+    const enableNotifications = await UserSettings.enableNotifications
+    this.setState({
+      enableNotifications
+    });
+  }
+
+  async _toggleNotifications() {
+    const { enableNotifications } = this.state
+    const value = !enableNotifications;
+    this.setState({
+      enableNotifications: value
+    });
+    UserSettings.enableNotifications = value;
+  }
+
+  render() {
+    return (
+      <View style={CommonStyles.pageContainer}>
+        <View style={PageStyles.form}>
+          <List>
+            <ListItem
+              title={I18n.t('account.settings.enableNotifications')}
+              hideChevron={true}
+              label={
+                <Switch
+                  value={this.state.enableNotifications}
+                  onValueChange={() => this._toggleNotifications()}
+                />
+              }
+            />
+          </List>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default Settings;
