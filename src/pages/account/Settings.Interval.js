203dcba7c66090b1cbf6651302d6a226acc5a893
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-02 00:09:10 +0300

    feat(settings): interval settings stub pages

---
+++ src/pages/account/Settings.Interval.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class SettingsInterval extends Component {
+  render() {
+    return (
+      <Text>Settings Inteval</Text>
+    );
+  }
+}
+
+export default SettingsInterval;
---
+++ src/pages/account/Settings.IntervalType.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class SettingsIntervalType extends Component {
+  render() {
+    return (
+      <Text>Settings Inteval Type</Text>
+    );
+  }
+}
+
+export default SettingsIntervalType;
