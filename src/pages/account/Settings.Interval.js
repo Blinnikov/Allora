a06e3bcccb7884c33fc885bb7d4c24013831b969
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-02 23:57:52 +0300

    feat(settings): interval settings page

--- src/pages/account/Settings.Interval.js
+++ src/pages/account/Settings.Interval.js
@@ -1,10 +1,97 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
+import UserSettings from '../../services/UserSettings';
+
+import CommonStyles from '../../styles/Common';
+import PageStyles from './Settings.Styles';
+
+const availableIntervals = ['1', '5', '10', '15', '30', '45', '60'];
+const availableTypes = ['minute', 'hour', 'day'];
+
+const checkIcon = <Icon name='ios-checkmark-outline' style={PageStyles.check} />;
 
 class SettingsInterval extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      interval: '',
+      intervalType: ''
+    };
+    this._fillInitialState()
+  }
+
+  async _fillInitialState() {
+    const interval = await UserSettings.notificationsInterval;
+    const intervalType = await UserSettings.notificationsIntervalType;
+    this.setState({
+      interval,
+      intervalType
+    });
+  }
+
+  _checkInterval(interval) {
+    UserSettings.notificationsInterval = interval;
+    this.setState({
+      interval
+    });
+  }
+
+  _checkIntervalType(intervalType) {
+    UserSettings.notificationsIntervalType = intervalType;
+    this.setState({
+      intervalType
+    });
+  }
+
   render() {
+    const { interval, intervalType } = this.state;
     return (
-      <Text>Settings Inteval</Text>
+      <View style={CommonStyles.pageContainer}>
+        <View style={PageStyles.form}>
+          <List>
+            {
+              availableIntervals.map(availableInterval => {
+                const label = availableInterval === interval
+                  ? checkIcon
+                  : null;
+
+                return (
+                  <ListItem key={availableInterval}
+                    title={availableInterval}
+                    onPress={() => this._checkInterval(availableInterval)}
+                    hideChevron={true}
+                    label={label}
+                    containerStyle={PageStyles.listItem}
+                  />
+                );
+              })
+            }
+          </List>
+          <List>
+            {
+              availableTypes.map(availableType => {
+                const label = availableType === intervalType
+                  ? checkIcon
+                  : null;
+
+                return (
+                  <ListItem
+                    key={availableType}
+                    onPress={() => this._checkIntervalType(availableType)}
+                    title={availableType}
+                    hideChevron={true}
+                    label={label}
+                    containerStyle={PageStyles.listItem}
+                  />
+                );
+              })
+            }
+          </List>
+        </View>
+      </View>
     );
   }
 }
--- src/pages/account/Settings.Styles.js
+++ src/pages/account/Settings.Styles.js
@@ -1,9 +1,25 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../constants';
 
 const SettingsStyles = StyleSheet.create({
   form: {
     flex: 1
   },
+  listItem: {
+    height: 44,
+    paddingTop: 12,
+    paddingBottom: 12
+  },
+  rightTitle: {
+    fontSize: 16,
+    color: '#999'
+  },
+  check: {
+    fontSize: 30,
+    alignSelf: 'center',
+    paddingRight: 10,
+    color: colors.primaryColor
+  }
 });
 
 export default SettingsStyles;
