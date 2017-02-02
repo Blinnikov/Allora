c82e460501ce3a047a0ed5783b2339367013b9e7
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-02 23:56:23 +0300

    feat(settings): add interval settings

--- src/services/UserSettings.js
+++ src/services/UserSettings.js
@@ -1,13 +1,29 @@
 import UserDefaults from 'react-native-userdefaults-ios';
 
-const enableNotificationsKey = 'settings.enableNotifications'
+const enableNotificationsKey = 'settings.notifications.enable';
+const notificationsIntervalKey = 'settings.notifications.interval';
+const notificationsIntervalTypeKey = 'settings.notifications.intervalType';
 
 const UserSettings = {
   get enableNotifications() {
-    return UserDefaults.boolForKey(enableNotificationsKey)
+    return UserDefaults.boolForKey(enableNotificationsKey);
   },
   set enableNotifications(value) {
     UserDefaults.setBoolForKey(value, enableNotificationsKey);
+  },
+
+  get notificationsInterval() {
+    return UserDefaults.stringForKey(notificationsIntervalKey);
+  },
+  set notificationsInterval(value) {
+    UserDefaults.setStringForKey(value, notificationsIntervalKey);
+  },
+
+  get notificationsIntervalType() {
+    return UserDefaults.stringForKey(notificationsIntervalTypeKey);
+  },
+  set notificationsIntervalType(value) {
+    UserDefaults.setStringForKey(value, notificationsIntervalTypeKey);
   }
 };
 
