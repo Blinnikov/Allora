d2f4cc097760a4ff1686d57f9d876f483944a2e0
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-11 21:15:10 +0300

    feat(styles): simplify stylings - remove padding for tabs and navbar since react-navigation calculates them by itself

--- src/styles/Common.js
+++ src/styles/Common.js
@@ -1,29 +1,17 @@
 import { StyleSheet } from 'react-native';
-import { sizes, colors } from '../constants';
+import { colors } from '../constants';
 
 const button = {
   marginLeft: 0,
   marginRight: 0
 };
 
-const fullHeightContainer = {
-  flex: 1,
-  flexDirection: 'column',
-  backgroundColor: colors.bodyBackgroundColor,
-  justifyContent: 'center'
-};
-
-const navBarContainer = {
-  ...fullHeightContainer,
-  paddingTop: sizes.statusBar + sizes.navigationBar
-};
-
 const Common = StyleSheet.create({
-  fullHeightContainer: {
-    ...fullHeightContainer
-  },
   pageContainer: {
-    ...navBarContainer,
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: colors.bodyBackgroundColor,
+    justifyContent: 'center'
   },
   buttonDefault: {
     ...button,
