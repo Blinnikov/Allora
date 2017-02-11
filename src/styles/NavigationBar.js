671ce4ab8f0e3991a77ed0cb560c6b8682ad2c78
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-11 21:37:52 +0300

    feat(navigation): remove obsolete navbar styles

--- src/styles/NavigationBar.js
+++ src/styles/NavigationBar.js
@@ -1,35 +1,19 @@
 import { StyleSheet } from 'react-native';
 import { colors } from '../constants';
 
+const navBarButtonIcon = {
+  fontSize: 24,
+  marginVertical: 8,
+  color: colors.primaryColor,
+};
+
 const NavigationBar = StyleSheet.create({
-  navBar: {
-    backgroundColor: colors.barsBackgroundColor,
-    borderWidth: 1,
-    borderColor: colors.transparent,
-    borderBottomColor: colors.$navBarBorderBottomColor,
-  },
-  navBarText: {
-    fontSize: 16,
-    marginVertical: 10,
-  },
-  navBarTitleText: {
-    color: colors.$navBarTitleTextColor,
-    fontWeight: '500',
-    marginVertical: 9,
-  },
-  navBarLeftButton: {
-    flexDirection: 'row',
-    paddingLeft: 20,
-  },
   navBarRightButton: {
+    paddingRight: 10,
+  },
+  navBarRightButtonIcon: {
+    ...navBarButtonIcon,
     paddingRight: 20,
-  },
-  navBarButtonText: {
-    color: colors.primaryColor,
-  },
-  navBarButtonIcon: {
-    fontSize: 24,
-    marginVertical: 8
   }
 });
 
