9b1d5bdeba225b59ad79a8ce4d254dcbb0d8b82b
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-22 22:15:20 +0300

    chore(firebase): some more courses methods

--- src/firebase/database/courses.js
+++ src/firebase/database/courses.js
@@ -1,7 +1,16 @@
 import { courses as ref } from './references';
 
+const add = (course) => {
+  ref().push(course);
+};
+
+const update = (key, course) => {
+  ref().child(key)
+    .update(course);
+};
+
 const remove = (key) => {
   ref().child(key).remove();
 };
 
-export { remove };
+export { add, update, remove };
