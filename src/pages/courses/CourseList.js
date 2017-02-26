7998cd7b054508aeee9092710ba1c85dc8895593
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-26 21:36:41 +0300

    chore(courses): activity indicator

--- src/pages/courses/CourseList.js
+++ src/pages/courses/CourseList.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
 
-const CourseList = () => (
-  <View>
-    <Text>Courses</Text>
-  </View>
-);
+import CommonStyles from '../../styles/Common';
+
+class CourseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this._data = null;
+
+    this.state = {
+      loading: true,
+      // dataSource: new ListView.DataSource({
+      //   rowHasChanged: (row1, row2) => row1 !== row2
+      // })
+    };
+  }
+  
+  render() {
+    if (this.state.loading) {
+      return <ActivityIndicator
+        size='large'
+        style={CommonStyles.pageContainer}
+      />;
+    }
+
+    return (
+      <View>
+        <Text>Courses</Text>
+      </View>
+    );
+  }
+}
 
 export default CourseList;
