51cbca11a60c69add0467a495ac0bfa2106df671
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2017-03-15 21:46:46 +0300

    chore: remove screenProps warning suppressing

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,7 @@
 import { AppRegistry } from 'react-native';
 import { storage }from 'allora-core';
 import './src/localization';
-
 import Root from './src/Root';
-
-/* eslint no-console: 0 */
-console.ignoredYellowBox = [
-   'Behaviour of screenProps has changed',
-];
 
 storage.init();
 
