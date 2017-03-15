86f0516b62d78c9c6e6c63b4e400f89c64faf2cb
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2017-03-15 22:57:13 +0300

    feat(logo): the same for SignUp page

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { NavigationActions } from 'react-navigation';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import { authentication } from 'allora-core';
 import I18n from 'react-native-i18n';
 import { FormInput } from 'react-native-elements';
@@ -8,6 +8,9 @@ import { Button } from 'react-native-elements';
 
 import PageStyles from './Login.Styles';
 import CommonStyles from '../../styles/Common';
+
+// TODO: For iPhone Plus - LogoBig, for others - Logo
+const logoImg = require('../../img/LogoBig.png');
 
 class SignUp extends Component {
   constructor(props) {
@@ -48,6 +51,7 @@ class SignUp extends Component {
   render() {
     return (
       <View style={[CommonStyles.pageContainer, PageStyles.loginPageContainer]}>
+        <Image style={PageStyles.logoBig} source={logoImg} />
         <FormInput
           value={this.state.email}
           autoCapitalize="none"
