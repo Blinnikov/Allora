6ff2fab529effbc71805a37c2dc2cdd00a954a79
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2017-04-18 22:16:43 +0300

    feat(flat list): basic flat list implementation. It works. And it's really faster :fire:

--- src/pages/words/WordFlatList.ios.js
+++ src/pages/words/WordFlatList.ios.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, FlatList } from 'react-native';
+import { storage } from 'allora-core';
+import ListItem from './components/ListItem';
 import WordsAddButton from '../../navigation/WordsAddButton';
+
+import CommonStyles from '../../styles/Common';
 
 class WordFlatList extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -9,11 +13,51 @@ class WordFlatList extends Component {
     };
   };
 
-  render() {
+  constructor(props) {
+    super(props);
+
+    this._data = null;
+
+    this.state = {
+      loading: true,
+      dataSource: null,
+    };
+  }
+
+  componentDidMount() {
+    storage.words.subscribe(this._setDataSource.bind(this));
+  }
+
+  _setDataSource(items) {
+    this._data = items;
+    this.setState({
+      loading: false,
+      dataSource: this._data,
+    });
+  }
+
+  _renderItem({ item }) {
     return (
-      <View>
-        <Text>Test</Text>
-      </View>
+      <ListItem
+        item={item}
+        onRemovePress={() => this._onRemoveButtonPress(item)}
+        onEditPress={() => this._onEditButtonPress(item)}
+      />
+    );
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <ActivityIndicator size="large" style={CommonStyles.pageContainer} />
+      );
+    }
+
+    return (
+      <FlatList
+        data={this.state.dataSource}
+        renderItem={this._renderItem}
+      />
     );
   }
 }
